8c4ed16a3c495d395d3bacd2f579ba81d73957ef
AdamKhelladi <khelladiadem5@gmail.com>
2025-08-23 11:52:26 +0100

    feat: implement useLocalStorage hook with JSON parse/stringify

--- src/components/QRCodeGenerator/UseLocalStorage.js
+++ src/components/QRCodeGenerator/UseLocalStorage.js
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
 
+export default function UseLocalStorage(key, defaultVlaue) {
+  const [value, setValue] = useState(() => {
+    let currentVlaue;
 
-export default function UseLocalStorage() {
-  
+    try {
+      currentVlaue = JSON.parse(
+        localStorage.getItem(key) || String(defaultVlaue)
+      );
+    } catch (error) {
+      console.log(error);
+      currentVlaue = defaultVlaue;
+    }
+
+    return currentVlaue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value))
+  }, [key, value])
+
+  return [value, setValue];
 }
--- src/components/ThemeSwitch/ThemeSwitch.js
+++ src/components/ThemeSwitch/ThemeSwitch.js
@@ -1,10 +1,26 @@
+import UseLocalStorage from "../QRCodeGenerator/UseLocalStorage";
 import "./ThemeSwitch.css";
 
 export default function ThemeSwitch() {
+  const [theme, setTheme] = UseLocalStorage("theme", "dark");
+
+  function handleChangeThemeClick() {
+    setTheme(theme === "dark" ? "light" : "dark")
+  }
+
+  console.log(theme);
+
   return (
-    <div className="theme-switch">
+    <div className="theme-switch" data-theme={theme}>
       <h1 className="theme-msg">Click to Change Theme!</h1>
-      <button className="change-theme-btn">Change Theme</button>
+      <button
+        className="change-theme-btn"
+        onClick={() => {
+          handleChangeThemeClick();
+        }}
+      >
+        Change Theme
+      </button>
     </div>
-  )
+  );
 }
