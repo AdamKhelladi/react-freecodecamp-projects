db189e86073ed9becba6a101145c6a3c42a88786
AdamKhelladi <khelladiadem5@gmail.com>
2025-08-24 11:27:35 +0100

    Add TabTest file

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
 import "./Tabs.css";
 
-export default function Tabs() {
+export default function Tabs({ tabsContent, onChange }) {
+
+  const [currentTabIndex, setCurrentTabIndex] = useState(0);
+
   return (
-    <div className="tabs-container">Tabs</div>
-  )
+    <div className="tabs-container">
+      <div className="heading">
+        {tabsContent.map((tabItem) => (
+          <div key={tabItem.label}>
+            <span className="label">{tabItem.label}</span>
+          </div>
+        ))}
+      </div>
+
+      <div className="content">
+        {
+          tabsContent[currentTabIndex] && tabsContent[currentTabIndex].content
+        }
+      </div>
+    </div>
+  );
 }
