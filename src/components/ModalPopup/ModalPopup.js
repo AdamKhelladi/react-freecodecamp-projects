f3a51826e34e07e036f3eb30c200a8eb67446de0
AdamKhelladi <khelladiadem5@gmail.com>
2025-08-25 11:40:45 +0100

    Add function to handle Close Modal Popup

--- src/components/ModalPopup/ModalPopup.js
+++ src/components/ModalPopup/ModalPopup.js
@@ -2,23 +2,44 @@ import { useState } from "react";
 import "./ModalPopup.css";
 
 export default function ModalPopup() {
-
   const [showModalPopup, setShowModalPopup] = useState(false);
+  const [showOpenModalBtn, setShowOpenModalBtn] = useState(true);
 
   function handleToggleModalPopup() {
     setShowModalPopup(!showModalPopup);
+    setShowOpenModalBtn(false);
+  }
+
+  function handleCloseModalPopup() {
+    setShowModalPopup(!showModalPopup);
+    setShowOpenModalBtn(true);
   }
 
   return (
     <div className="modal-popup-container">
-      <button className="modal-popup-btn" onClick={() => {
-        handleToggleModalPopup();
-      }}>Open Model Popup</button>
-      {
-        showModalPopup ? 
-        <div className="box-modal">Modal Popup Shows Here..</div>
-        : null
-      }
+      {showOpenModalBtn ? (
+        <button
+          className="open-modal-btn"
+          onClick={() => {
+            handleToggleModalPopup();
+          }}
+        >
+          Open Modal Popup
+        </button>
+      ) : null}
+      {showModalPopup ? (
+        <div className="box-modal">
+          <p>Modal Popup Shows Here..</p>
+          <button
+            className="close-modal-btn"
+            onClick={() => {
+              handleCloseModalPopup();
+            }}
+          >
+            Close Modal Popup
+          </button>
+        </div>
+      ) : null}
     </div>
-  )
+  );
 }
