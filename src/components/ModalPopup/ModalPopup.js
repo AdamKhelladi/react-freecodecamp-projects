6ad80c63e5e67b6dfccd8528644d6be84c83a180
AdamKhelladi <khelladiadem5@gmail.com>
2025-08-25 11:24:34 +0100

    feat: add ModalPopup component with toggle functionality

--- src/components/ModalPopup/ModalPopup.js
+++ src/components/ModalPopup/ModalPopup.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import "./ModalPopup.css";
 
 export default function ModalPopup() {
+
+  const [showModalPopup, setShowModalPopup] = useState(false);
+
+  function handleToggleModalPopup() {
+    setShowModalPopup(!showModalPopup);
+  }
+
   return (
-    <div className="modal-popup-container">Modal Popup</div>
+    <div className="modal-popup-container">
+      <button className="modal-popup-btn" onClick={() => {
+        handleToggleModalPopup();
+      }}>Open Model Popup</button>
+      {
+        showModalPopup ? 
+        <div className="box-modal">Modal Popup Shows Here..</div>
+        : null
+      }
+    </div>
   )
 }
