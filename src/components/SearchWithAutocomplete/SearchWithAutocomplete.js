044e41542b05b255ea9fc0aa51c841d1ac2e4c3c
AdamKhelladi <khelladiadem5@gmail.com>
2025-08-26 11:46:00 +0100

    Add Styles to component

--- src/components/SearchWithAutocomplete/SearchWithAutocomplete.js
+++ src/components/SearchWithAutocomplete/SearchWithAutocomplete.js
@@ -2,6 +2,15 @@ import "./SearchWithAutocomplete.css";
 
 export default function SearchWithAutocomplete() {
   return (
-    <div className="autocomplete-container">Search With Autocomplete</div>
-  )
+    <div className="autocomplete-container">
+      <div className="the-input-wrapper">
+        <input
+          name="search-by-username"
+          type="text"
+          placeholder="Enter GitHub Username"
+        />
+        <button className="search-btn">Search</button>
+      </div>
+    </div>
+  );
 }
